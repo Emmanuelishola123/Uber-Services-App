cd263f07dd00d32d27285e21e4d389a80fb00138
emmanuelishola123 <emmanuelishola7@gmail.com>
2023-08-17 05:25:23 +0100

    init new project - Uber App

---
+++ redux/slices/navSlice.js
@@ -0,0 +1,29 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInfo: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInfo: (state, action) => {
+      state.traveTimeInfo = action.payload;
+    },
+  },
+});
+
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInfo = (state) => state.nav.travelTimeInfo;
+
+export default navSlice.reducer;
